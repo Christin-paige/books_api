42b2270f8af9813e2052cf8f6e93f624755b2f28
Christin-paige <christinpmartin@gmail.com>
2024-08-05 13:54:57 -0400

    added dotenv file

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'books',
-    password: 'Harper',
-    port: 5433,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
+
 module.exports = pool;
